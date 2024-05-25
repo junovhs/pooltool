2de8c84b8851564463385b18ef4f7bf60ddccc14
junovhs <125226615+junovhs@users.noreply.github.com>
2024-05-25 16:42:09 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,23 +5,17 @@ const pages = [
 ];
 
 function loadNavbar() {
-    console.log('loadNavbar called'); // Debugging log
     fetch('https://junovhs.github.io/pooltool/components/navbar.html')
-        .then(response => {
-            console.log('fetch response received'); // Debugging log
-            return response.text();
-        })
+        .then(response => response.text())
         .then(data => {
             document.getElementById('navbar-placeholder').innerHTML = data;
             const navLinks = document.getElementById('nav-links');
-            console.log('navLinks:', navLinks); // Debugging log
             pages.forEach(page => {
                 if (page.enabled) {
                     const li = document.createElement('li');
                     li.className = 'nav-item';
                     li.innerHTML = `<a class="nav-link" href="https://junovhs.github.io/pooltool/${page.path}">${page.name}</a>`;
                     navLinks.appendChild(li);
-                    console.log(`Added link for ${page.name}`); // Debugging log
                 }
             });
         });
