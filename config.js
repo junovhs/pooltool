c4138ef700a893ffb1dc7e29545fce9cadb4210a
junovhs <125226615+junovhs@users.noreply.github.com>
2024-05-25 16:55:26 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const pages = [
     { name: "Home", path: "index.html", enabled: true },
     { name: "QR Code Generator", path: "pages/qr.html", enabled: true },
+    { name: "Text Encryption Tool", path: "pages/encryption.html", enabled: true },
     // Add more pages as needed
 ];
 
