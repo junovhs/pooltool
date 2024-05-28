7f4552d9dac0c4273447cfcb12901e256641ec3a
junovhs <125226615+junovhs@users.noreply.github.com>
2024-05-27 17:00:34 -0700

    Update scripts.js

--- components/scripts.js
+++ components/scripts.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     loadNavbar();
 });
+// Add this function to scripts.js
+function generateUniqueEmail() {
+    const randomString = Math.random().toString(36).substring(2, 10);
+    const email = `${randomString}@tempmail.com`;
+    document.getElementById('email-display').innerText = email;
+}
+
+document.getElementById('generate-email').addEventListener('click', generateUniqueEmail);
