e1323700ea386d8a432d527f5d95a6180e6ef45e
junovhs <125226615+junovhs@users.noreply.github.com>
2024-07-20 19:38:41 -0700

    Create auto-inject.js

---
+++ auto-inject.js
@@ -0,0 +1,8 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = 'https://junovhs.github.io/pooltool/config.js';
+    script.onload = function() {
+        loadNavbar();
+    };
+    document.head.appendChild(script);
+})();
