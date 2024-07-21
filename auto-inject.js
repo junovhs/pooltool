ed473d37a31a96fcf06a6c7c15c4d686728afefe
junovhs <125226615+junovhs@users.noreply.github.com>
2024-07-20 19:43:03 -0700

    Delete auto-inject.js

--- auto-inject.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var script = document.createElement('script');
-    script.src = 'https://junovhs.github.io/pooltool/config.js';
-    script.onload = function() {
-        loadNavbar();
-    };
-    document.head.appendChild(script);
-})();
