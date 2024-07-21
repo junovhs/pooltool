0bae9d2a6664c01d6c5996fef70139d69932082e
junovhs <125226615+junovhs@users.noreply.github.com>
2024-07-20 19:29:11 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,29 @@
-function loadNavbar() {
-    fetch('https://junovhs.github.io/pooltool/pages-list.json')
-        .then(response => response.json())
-        .then(pages => {
-            fetch('https://junovhs.github.io/pooltool/components/navbar.html')
-                .then(response => response.text())
-                .then(data => {
-                    document.getElementById('navbar-placeholder').innerHTML = data;
-                    const navLinks = document.getElementById('nav-links');
-                    pages.forEach(page => {
-                        if (page.enabled) {
-                            const li = document.createElement('li');
-                            li.className = 'nav-item';
-                            li.innerHTML = `<a class="nav-link" href="https://junovhs.github.io/pooltool/${page.path}">${page.name}</a>`;
-                            navLinks.appendChild(li);
-                        }
-                    });
-                });
+async function getPagesList() {
+    const response = await fetch('https://api.github.com/repos/junovhs/pooltool/contents/pages');
+    const files = await response.json();
+    return files
+        .filter(file => file.name.endsWith('.html'))
+        .map(file => ({
+            name: file.name.replace('.html', '').replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase()),
+            path: `pages/${file.name}`,
+            enabled: true
+        }));
+}
+
+async function loadNavbar() {
+    const pages = await getPagesList();
+    fetch('https://junovhs.github.io/pooltool/components/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-placeholder').innerHTML = data;
+            const navLinks = document.getElementById('nav-links');
+            pages.forEach(page => {
+                if (page.enabled) {
+                    const li = document.createElement('li');
+                    li.className = 'nav-item';
+                    li.innerHTML = `<a class="nav-link" href="https://junovhs.github.io/pooltool/${page.path}">${page.name}</a>`;
+                    navLinks.appendChild(li);
+                }
+            });
         });
 }
