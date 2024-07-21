22e06d14cb3b549368eda615176ad2f4c5e2bf2c
junovhs <125226615+junovhs@users.noreply.github.com>
2024-07-20 19:27:50 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,20 @@
-const pages = [
-  { name: "Home", path: "index.html", enabled: true },
-  { name: "QR Code Generator", path: "pages/qr.html", enabled: true },
-  { name: "Salvia Dosage Calculator", path: "pages/salviadosage.html", enabled: true },
-  // Add more pages as needed
-];
-
 function loadNavbar() {
-  fetch('https://junovhs.github.io/pooltool/components/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('navbar-placeholder').innerHTML = data;
-      const navLinks = document.getElementById('nav-links');
-      pages.forEach(page => {
-        if (page.enabled) {
-          const li = document.createElement('li');
-          li.className = 'nav-item';
-          li.innerHTML = `<a class="nav-link" href="https://junovhs.github.io/pooltool/${page.path}">${page.name}</a>`;
-          navLinks.appendChild(li);
-        }
-      });
-    });
+    fetch('https://junovhs.github.io/pooltool/pages-list.json')
+        .then(response => response.json())
+        .then(pages => {
+            fetch('https://junovhs.github.io/pooltool/components/navbar.html')
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('navbar-placeholder').innerHTML = data;
+                    const navLinks = document.getElementById('nav-links');
+                    pages.forEach(page => {
+                        if (page.enabled) {
+                            const li = document.createElement('li');
+                            li.className = 'nav-item';
+                            li.innerHTML = `<a class="nav-link" href="https://junovhs.github.io/pooltool/${page.path}">${page.name}</a>`;
+                            navLinks.appendChild(li);
+                        }
+                    });
+                });
+        });
 }
